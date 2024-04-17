43d67bf6773ca56388649974dec6fc730a7e39c0
Jeffrey Elkner <jeff.elkner@novawebdevelopmentl.org>
2024-04-17 14:35:49 -0400

    Add homepage to tiny app

--- tinysite/tinyapp.py
+++ tinysite/tinyapp.py
@@ -1,5 +1,19 @@
+from django.urls import re_path 
+from django.http import HttpResponse
+
+
 DEBUG = True
 SECRET_KEY = '4l0ngs3cr3tstr1ngw3lln0ts0l0ngw41tn0w1tsl0ng3n0ugh'
 ROOT_URLCONF = __name__
 
-urlpatterns = []
+
+def index(request):
+    f = open('index.html', 'r')
+    page = f.read()
+    f.close()
+    return HttpResponse(page)
+
+
+urlpatterns = [
+    re_path(r'^$', index),
+]
