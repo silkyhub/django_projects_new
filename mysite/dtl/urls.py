3970c356da95b3a727ae9dd6c13cd79ff6afb18a
Jeffrey Elkner <jeff.elkner@gmail.com>
2025-02-11 09:09:55 -0500

    Make bottles work!

--- mysite/dtl/urls.py
+++ mysite/dtl/urls.py
@@ -6,5 +6,11 @@ from django.views.generic import TemplateView
 app_name = 'dtl'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dtl/index.html')),
-    path('bottles', views.bottles),
+    path('bottles', views.bottles, name="bottles"),
+    path('bottles/<str:drink>', views.bottles, name="bottles_drink"),
+    path(
+        'bottles/<str:drink>/<int:num_bottles>',
+        views.bottles,
+        name="bottles_drink_num_bottles",
+    ),
 ]    
--- mysite/dtl/views.py
+++ mysite/dtl/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def bottles(request):
-    context = {'drink' : 'pop' }
+def bottles(request, drink='pop', num_bottles=100):
+    context = {
+        'drink': drink,
+        'num_bottles': str(num_bottles),
+        'n': range(num_bottles, 1, -1)
+    }
     return render(request, 'dtl/song.html', context)
